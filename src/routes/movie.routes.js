a98d3c1b489d202c64932de5b1f756e58f075210
Viviana Gonzalez <vivigz06@gmail.com>
2025-06-27 12:51:18 -0300

    Se realizaron las rutas

--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -6,5 +6,11 @@ import { getMovies, getMoviesId, postMovie,deleteMovies
 
 const router = express.Router()
 
+router.get('/api/movies', getMovies); // Devolver todos los personajes registrados
+router.get('/api/movies/:id', getMoviesId); // Devolver un personaje por su id
+router.post('/api/movies', createCharacters); // Crear un nuevo personaje
+router.put('/api/movies/:id', updateCharacter); // Actualizar los datos de un personaje existente con
+router.delete('/api/movies/:id', deleteCharacter); //
+
 
 export default router
